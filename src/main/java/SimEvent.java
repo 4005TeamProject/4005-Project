75fe676e576dd54921790647268c78b92077c474
Simon <simonyacoub@cmail.carleton.ca>
2021-02-28 19:10:28 -0500

    Added SimEvent
    SimEvent Describes a simulation event

---
+++ src/main/java/SimEvent.java
@@ -0,0 +1,39 @@
+public class SimEvent implements Comparable<SimEvent> {
+
+    /*
+     * ALS1=Arrival at Inspector1, ALS2=Arrival at Inspector2,
+     * ELS1=End of inspection at Inspector1, ELS2=End of inspection at Inspector2
+     * ALW1=Arrival at workstation 1, ALW2=Arrival at workstation 2, ALW3=Arrival at workstation 3
+     * ELW1=Product exits workstation 1, ELW2=Product exits workstation 2, ELW3=Product exits workstation 3
+     * ES=End of Simulation
+     */
+    public static enum eventType {ALS1, ALS2, ELS1,ELS2,ALW1, ALW2, ALW3, ELW1, ELW2, ELW3, ES};
+    private eventType eventType;        // Type of the event
+    private Integer eventTime;          // Event Time
+
+    public SimEvent(eventType eventType, int eventTime) {
+        this.eventType = eventType;
+        this.eventTime = eventTime;
+    }
+
+    @Override
+    public int compareTo(SimEvent event) {
+        return this.getEventTime().compareTo(event.getEventTime());
+    }
+
+    public eventType geteventType() {
+        return eventType;
+    }
+
+    public void setEventType(eventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public Integer getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(int eventTime) {
+        this.eventTime = eventTime;
+    }
+}
