d91f250f8d42932f927d76844edd4e51d1d452c1
Simon <simonyacoub@cmail.carleton.ca>
2021-04-10 12:26:18 -0400

    Added component entity class

---
+++ src/main/java/Component.java
@@ -0,0 +1,42 @@
+public class Component {
+
+    /*
+    Inspection is when component enters inspection, buffer is when it is done inspection and
+    placed in a buffer to a workstation and workstation is when the component is consumed and a
+    product is created.
+     */
+    public enum serviceType {Inspection, Buffer, Workstation}
+
+    private int componentID;
+    private int componentNumber; // {1, 2, 3}
+    private serviceType currentLocation;
+
+    public Component(int id, int componentNumber){
+        this.componentNumber = componentNumber;
+        this.componentID = id;
+    }
+
+    public int getComponentID() {
+        return componentID;
+    }
+
+    public void setComponentID(int id){
+        this.componentID = id;
+    }
+
+    public int getComponentNumber() {
+        return componentNumber;
+    }
+
+    public void setComponentNumber(int componentNumber) {
+        this.componentNumber = componentNumber;
+    }
+
+    public serviceType getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(serviceType currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+}
