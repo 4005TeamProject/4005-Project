b2df837adad31e9e46ae6ece897f14e04a1d4431
Simon <simonyacoub@cmail.carleton.ca>
2021-04-10 14:39:08 -0400

    Added BUF SimEvent for buffering componenets

--- src/main/java/SimEvent.java
+++ src/main/java/SimEvent.java
@@ -3,12 +3,13 @@ public class SimEvent implements Comparable<SimEvent> {
     /*
      * ALS1=Arrival at Inspector1, ALS2=Arrival at Inspector2,
      * ELS1=End of inspection at Inspector1, ELS2=End of inspection at Inspector2
+     * BUF = Buffering between inspection and workstation
      * ALW1=Arrival at workstation 1, ALW2=Arrival at workstation 2, ALW3=Arrival at workstation 3
      * ELW1=Product exits workstation 1, ELW2=Product exits workstation 2, ELW3=Product exits workstation 3
      * ES=End of Simulation
      */
 
-    public static enum eventType {ALS1, ALS2, ELS1,ELS2,ALW1, ALW2, ALW3, ELW1, ELW2, ELW3, ES};
+    public static enum eventType {ALS1, ALS2, BUF, ELS1,ELS2,ALW1, ALW2, ALW3, ELW1, ELW2, ELW3, ES};
     private eventType eventType;        // Type of the event
     private Double eventTime;          // Event Time
 
